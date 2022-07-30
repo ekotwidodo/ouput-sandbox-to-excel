0799a58062351d3ac01af50a874f059d63f7277b
Eko Teguh Widodo <id.ekoteguh@gmail.com>
2022-07-30 23:29:02 +0700

    Update mekanisme export ke excel tanpa perlu dibuat di server, tambah total records dan judul tabel

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,4 +1,3 @@
-const excel = require('exceljs')
 const fs = require('fs')
 
 const writeQuery = async (query, filename) => {
@@ -8,24 +7,6 @@ const writeQuery = async (query, filename) => {
   } catch (err) {
     console.log(err)
   }
-} 
-
-const formatJSON = (string) => {
-  let formattedJSON = []
-  // Get headers
-  let headers = string.split('\n')[0].split('\t')
-  // Get rows
-  for (let i = 1; i < string.split('\n').length-1; i++) {
-      let rows = {}
-      for (let j = 0; j < string.split('\n')[i].split('\t').length; j++) {
-          let items = string.split('\n')[i].split('\t')
-          rows[headers[j]] = items[j]
-      }
-
-      formattedJSON.push(rows)
-  }
-
-  return formattedJSON
 }
 
 const handleSpecialCharacters = (text) => {
@@ -37,44 +18,27 @@ module.exports = {
 
     index(req, res){
         res.render('index', {
-            pageTitle: 'Export JSON ke Excel (Output Sandbox)',
-            pageSubtitle: 'Aplikasi ini dibangun dan dikembangkan oleh',
-            pageAuthorName: 'Eko Teguh Widodo',
-            pageAuthorUrl: 'https://github.com/ekotwidodo/output-sandbox-to-excel'
+            pageTitle: process.env.PAGE_TITLE,
+            pageSubtitle: process.env.PAGE_SUBTITLE,
+            pageDescription: process.env.PAGE_DESCRIPTION,
+            footerDescription: process.env.FOOTER_DESCRIPTION,
+            footerAuthorName: process.env.FOOTER_AUTHOR,
+            footerAuthorUrl: process.env.FOOTER_AUTHOR_URL
         })
     },
 
     export(req, res){
         // Ambil data dari form
         const data = JSON.parse(req.body.data)
-        // Ambil output dan format ke dalam bentuk JSON
-        const jsonData = formatJSON(data.results.msg[1].data)
         // Ambil title dan query
         const title = data.title
         const query = data.text
         // Set timestamp
         const timestamp = new Date().getTime()
-        // Setting excel
-        const workbook = new excel.Workbook()
-        const sheet = workbook.addWorksheet('data')
-        // Masukkan datanya
-        sheet.addRow().values = Object.keys(jsonData[0])
-        jsonData.forEach(function(row) {
-            let valRow = []
-            valRow = Object.values(row)
-            sheet.addRow().values = valRow
-        })
-
         // Buat nama file
         const filename = handleSpecialCharacters(`${title.split(' ').join('_')}_${timestamp}`)
-
         // 1. Simpan log query ke dalam folder query
         writeQuery(query, filename)
-
-        // 2. Buat file excel dan simpan ke dalam files
-        workbook.xlsx.writeFile(`./public/files/${filename}.xlsx`)
-            .then(() => {
-                res.status(200).json({ status: true, filename: filename })
-        })
+        res.status(200).json({ status: true, filename: filename })
     }
 }
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,11 +1,8 @@
 function handleChange (event, directValue) {
-
     // Get the value from jsonString input
     let jsonString= event.target.value
-
     // Parse jsonString into JSON formatted
     let jsonFormat = JSON.parse(jsonString)
-
     // Show the JSON formatted in jsonFormat input
     let jsonViewer = document.getElementById('jsonFormat')
 
@@ -27,8 +24,9 @@ function handleChange (event, directValue) {
 
     // Show the JSON selected data into table
     let data = jsonFormat.data.results.msg[1].data
+    let title = jsonFormat.data.title
     setJSONData(jsonFormat.data)
-    showTable(formatJSON(data))
+    showTable(formatJSON(data), title)
 }
 
 function setJSONData(data) {
@@ -36,8 +34,7 @@ function setJSONData(data) {
     jsonData.value = JSON.stringify(data)
 }
 
-function showTable(data) {
-
+function showTable(data, title) {
     // Extract value for html header
     let col = []
     for (let i = 0; i < data.length; i++) {
@@ -47,37 +44,49 @@ function showTable(data) {
             }
         }
     }
-
     // Create dynamic table
     let table = document.createElement("table")
     table.className = "table striped table-border mt-4"
+    table.setAttribute("id", "dataTable")
+    table.setAttribute("width", "100%")
+    table.setAttribute("cellspacing", "0")
 
     // Create html table header row using the extracted headers above
-    let tr = table.insertRow(-1)
+    let header = table.createTHead()
+    let row = header.insertRow(-1)
 
     for (let j = 0; j < col.length; j++) {
         let th = document.createElement("th")
         th.innerHTML = col[j]
-        tr.appendChild(th)
+        row.appendChild(th)
     }
-
+    let totalRecord = 0
     // Add JSON data to the tables as rows
+    let body = table.createTBody()
     for (let k = 0; k < data.length; k++) {
-        tr = table.insertRow(-1)
+        tr = body.insertRow(-1)
         for (let l = 0; l < col.length; l++) {
             let tabCell = tr.insertCell(-1)
             tabCell.innerHTML = data[k][col[l]]
         }
+        totalRecord+=1
     }
-
     // Finally add the newlu created table 
     let divContainer = document.getElementById("showTable")
     divContainer.innerHTML = ""
+    // add title before table
+    let p = document.createElement("p")
+    p.className = "text-center"
+    p.innerHTML = "<strong>" + title + "</strong>"
+    divContainer.appendChild(p)
+    // append table
     divContainer.appendChild(table)
+    // add total records
+    let pContainer = document.getElementById("totalRecord")
+    pContainer.innerHTML = "Total Record: <b>" + totalRecord + " records</b>"
 }
 
 function formatJSON(string) {
-    
     let formattedJSON = []
     // Get headers
     let headers = string.split('\n')[0].split('\t')
@@ -88,15 +97,20 @@ function formatJSON(string) {
             let items = string.split('\n')[i].split('\t')
             rows[headers[j]] = items[j]
         }
-
         formattedJSON.push(rows)
     }
-
     return formattedJSON
 }
 
+// Add download excel function using XLSX library
+function downloadExcel(filename) {
+    let table = document.getElementById("dataTable")
+    let file = XLSX.utils.table_to_book(table, {sheet: "data"})
+    XLSX.write(file, {bookType: 'xlsx', bookSST: true, type: 'base64'})
+    XLSX.writeFile(file, `${filename}.xlsx`)
+}
+
 async function postData (url, data) {
-    
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -120,8 +134,20 @@ formExport.addEventListener("submit", function(event) {
     postData('/export', formData)
         .then(result => {
             if (result.status) {
-                window.location = `./download/${result.filename}.xlsx`
+                downloadExcel(result.filename)
             }
         })
         .catch(err => console.log(err))
 })
+
+function handleInfo(footerDescription, footerAuthorName, footerAuthorUrl) {
+    let infoContent = `
+    <h3>Informasi Aplikasi</h3>
+    <p>${footerDescription}</p>
+    <p>
+        Pembangunan dan pengembangan aplikasi tersebut dapat dilihat melalui <a href="${footerAuthorUrl}" target="_blank" style="text-decoration:none;">Link Github</a>.
+    </p>
+    <p>-${footerAuthorName}</p>
+    `
+    Metro.infobox.create(infoContent)
+}
